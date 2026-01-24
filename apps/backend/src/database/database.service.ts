e94be7d4b94fc50332e1f366fc222ccf8e21c341
Girgis <girgisemad74@gmail.com>
2026-01-24 14:47:22 +0200

    add neon setup for db and drizzle worked

---
+++ apps/backend/src/database/database.service.ts
@@ -0,0 +1,21 @@
+import { neon } from '@neondatabase/serverless';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import * as schema from '../drizzle/schema.js';
+
+@Injectable()
+export class DatabaseService {
+  public readonly db: NeonHttpDatabase<typeof schema>;
+
+  constructor(private configService: ConfigService) {
+    const databaseUrl = this.configService.get<string>('DATABASE_URL');
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is missing!');
+    }
+    const sql = neon(databaseUrl);
+
+    this.db = drizzle(sql, { schema });
+  }
+}
