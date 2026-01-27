74ef4f80f5914e3e8e7debb97d71cc025efb3ebd
Patrick <rekoshehata@gmail.com>
2026-01-27 19:16:23 +0200

    Added sections and machines

--- apps/backend/src/drizzle/schema.ts
+++ apps/backend/src/drizzle/schema.ts
@@ -1,10 +1,20 @@
-import { pgEnum, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+import { pgEnum, pgTable, serial, text, varchar, timestamp, integer, boolean } from 'drizzle-orm/pg-core';
 
 export const roleEnum = pgEnum('role_enum', ['INTERN', 'ENGINEER', 'ADMIN']);
 
-export const users = pgTable('users', {
+export const sections = pgTable('sections', {
   id: serial('id').primaryKey(),
-  name: text('name'),
-  email: varchar('email', { length: 256 }).unique(),
-  role: roleEnum('role').default('INTERN'),
+  name: text('name').notNull(),
+  location: text('location'),
+  createdDate: timestamp('created_date').defaultNow(),
 });
+
+export const machines = pgTable('machines', {
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  hospCode: text('hosp_code'),
+  sectionId: integer('section_id')
+    .references(() => sections.id)
+    .notNull(), 
+});
+
