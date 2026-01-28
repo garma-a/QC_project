5b25c9a8f4a06a7cb77d856de6acb7c7ab3c2251
Girgis <girgisemad74@gmail.com>
2026-01-28 15:43:06 +0200

    add machine module and API docs for it

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { MachinesModule } from './machines/machines.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     DatabaseModule,
+    MachinesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
