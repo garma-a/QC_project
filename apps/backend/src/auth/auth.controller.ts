7671e6ad4df0417826386d68be1a47a41ac7ce8b
Patrick <rekoshehata@gmail.com>
2026-02-11 01:53:55 +0200

    Finished the auth controller

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService){}
 
     @Post('sign-up')
-    signUp(){
+    signUp(@Body () signUpDto: SignUpDto){
         return this.authService.signUp();
     }
      @Post('login')
-    login(){
+    login(@Body() loginUpDto: LoginDto){
         return this.authService.login();
     }
 }
