b8edee827e2c8961f3a9844cbfd151d85f405470
Patrick <rekoshehata@gmail.com>
2026-02-10 21:52:40 +0200

    Added ValidationPipe in main.ts

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,14 +1,28 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AllExceptionsFilter } from './http-exception.filter.js';
 import { Logger } from '@nestjs/common';
+
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+
   const httpAdapter = app.get(HttpAdapterHost);
+
+  app.useGlobalPipes(
+  new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true, 
+  }),
+);
+
   const config = new DocumentBuilder()
     .setTitle('QC-Project API')
     .setDescription('The Full documnetation to the QC-Project API')
