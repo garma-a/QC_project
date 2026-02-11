9a3460308dd65f98cd713e6a4385689dbe4920c4
Patrick <rekoshehata@gmail.com>
2026-02-11 07:25:45 +0200

    Adding the Guards in the users controller

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { AdminCreateUserDto } from './dto/admin-create-user.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
 
 
 @Controller('users')
@@ -10,6 +12,8 @@ export class UsersController {
 
   
     @Post()
+    @UseGuards(JwtAuthGuard,RolesGuard)
+    @Roles('ADMIN')
        async createUser(@Body() adminCreateUserDto: AdminCreateUserDto){
                 return await this.userService.createUser(adminCreateUserDto);
 
