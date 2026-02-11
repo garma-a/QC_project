3ccbd47745b87882d6c1893c7062e12030dc3e45
Patrick <rekoshehata@gmail.com>
2026-02-11 06:02:47 +0200

    Finished building the auth service

--- apps/backend/src/auth/auth.service.ts
+++ apps/backend/src/auth/auth.service.ts
@@ -1,12 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { users } from '../drizzle/schema';
+import { eq } from 'drizzle-orm';
+import * as argon2 from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-    signUp(){
-        return 'test';
-    }
+  constructor( private databaseService: DatabaseService,private jwtService: JwtService) {}
 
-    login(){
-        return 'test';
-    }
+  async login(loginDto: LoginDto) {
+   
+    const [user] = await this.databaseService.db
+      .select()
+      .from(users)
+      .where(eq(users.email, loginDto.email));
+
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+   
+    const passwordMatches = await argon2.verify(user.passwordHash, loginDto.password);
+    if (!passwordMatches) throw new UnauthorizedException('Invalid credentials');
+
+    
+    const payload = { sub: user.id, role: user.role };
+    return { access_token: this.jwtService.sign(payload) };
+  }
 }
