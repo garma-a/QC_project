b95f5f698c6d70a65196e70d0961b8a41e717445
Patrick <rekoshehata@gmail.com>
2026-02-11 04:23:55 +0200

    Finished the createUser function in the user-services

--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { AdminCreateUserDto } from './dto/admin-create-user.dto';
+import { users } from '../drizzle/schema'; 
+import * as argon2 from 'argon2';
+import { DatabaseService } from 'src/database/database.service';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private databaseService: DatabaseService) {}
+
+   async createUser(adminCreateUserDto: AdminCreateUserDto){
+        const hashedPassword = await argon2.hash(adminCreateUserDto.password);
+            
+    const existing = await this.databaseService.db
+     .select()
+     .from(users)
+    .where(eq(users.email, adminCreateUserDto.email));
+
+    if (existing.length > 0) {
+    throw new ConflictException('Email already exists');
+    }
+
+   const [createdUser] = await this.databaseService.db
+    .insert(users)
+    .values({
+      firstName: adminCreateUserDto.firstName,
+      lastName: adminCreateUserDto.lastName,
+      email: adminCreateUserDto.email,
+      passwordHash: hashedPassword,
+      role: adminCreateUserDto.role ?? 'INTERN',
+      isActive: adminCreateUserDto.isActive ?? true,
+    })
+    .returning();
+    const { passwordHash, ...safeUser } = createdUser;
+    return safeUser;
+
+}
+}
