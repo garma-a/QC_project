7342cb2d2dd9567b588d8581ff8e36391eaa0f55
Patrick <rekoshehata@gmail.com>
2026-02-11 04:45:45 +0200

    Made the endpoint for the createUser function

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { AdminCreateUserDto } from './dto/admin-create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService){}
+
+  
+    @Post('create')
+       async createUser(@Body() adminCreateUserDto: AdminCreateUserDto){
+                return await this.userService.createUser(adminCreateUserDto);
+
+        }
+}
