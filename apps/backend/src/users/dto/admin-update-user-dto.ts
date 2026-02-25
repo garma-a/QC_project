032bdbdba06cc8cfce63b370ffaedf014736347c
Patrick <rekoshehata@gmail.com>
2026-02-25 21:28:37 +0200

    Added the update dto

---
+++ apps/backend/src/users/dto/admin-update-user-dto.ts
@@ -0,0 +1,25 @@
+import { IsEmail, IsOptional, MinLength, IsEnum } from 'class-validator';
+import { UserRole } from './admin-create-user.dto';
+
+export class AdminUpdateUserDto {
+  @IsOptional()
+  firstName?: string;
+
+  @IsOptional()
+  lastName?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @MinLength(8)
+  password?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?: UserRole;
+
+  @IsOptional()
+  isActive?: boolean;
+}
