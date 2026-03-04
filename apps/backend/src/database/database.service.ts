dac87aafc80db7fd559f8269dea44d1276959ad3
Girgis <girgisemad74@gmail.com>
2026-03-04 03:45:21 +0200

    switch to neon

--- apps/backend/src/database/database.service.ts
+++ apps/backend/src/database/database.service.ts
@@ -1,16 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { drizzle, NeonHttpDatabase } from 'drizzle-orm/neon-http';
 
-
-import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
 
 import * as schema from '../drizzle/schema';
+import { neon } from '@neondatabase/serverless';
 
 @Injectable()
 export class DatabaseService {
-  // Switch the type to PostgresJsDatabase
-  public readonly db: PostgresJsDatabase<typeof schema>;
+  public readonly db: NeonHttpDatabase<typeof schema>;
 
   constructor(private configService: ConfigService) {
     const databaseUrl = this.configService.get<string>('DATABASE_URL');
@@ -18,9 +16,8 @@ export class DatabaseService {
     if (!databaseUrl) {
       throw new Error('DATABASE_URL is missing!');
     }
+    const sql = neon(databaseUrl);
 
-    // Use postgres.js for local TCP connection
-    const queryClient = postgres(databaseUrl);
-    this.db = drizzle(queryClient, { schema });
+    this.db = drizzle(sql, { schema });
   }
 }
