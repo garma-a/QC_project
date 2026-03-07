ac01381ee35442f24673cbf127dd02201a795884
Patrick <rekoshehata@gmail.com>
2026-03-07 21:06:13 +0200

    Creating te contorllot controllers endpoints

--- apps/backend/src/control-lots/control-lots.controller.ts
+++ apps/backend/src/control-lots/control-lots.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { ControlLotsService } from './control-lots.service';
+import { CreateControlLotDto } from './dto/create-control-lot.dto';
+import { UpdateControlLotDto } from './dto/update-control-lot.dto';
 
 @Controller('control-lots')
-export class ControlLotsController {}
+export class ControlLotsController {
+    constructor(private readonly controlLotsService: ControlLotsService) { }
+
+    @Post()
+    create(@Body(new ValidationPipe()) createControlLotDto: CreateControlLotDto) {
+        return this.controlLotsService.create(createControlLotDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.controlLotsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.controlLotsService.findOne(+id);
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body(new ValidationPipe()) updateControlLotDto: UpdateControlLotDto) {
+        return this.controlLotsService.update(+id, updateControlLotDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.controlLotsService.remove(+id);
+    }
+}
