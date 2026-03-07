e574342d15d646a0a62c58b1253beb8b6dd63c25
Patrick <rekoshehata@gmail.com>
2026-03-07 20:38:42 +0200

    Done with the create functions to add the controlLots in the service

--- apps/backend/src/control-lots/control-lots.service.ts
+++ apps/backend/src/control-lots/control-lots.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '@/database/database.service';
+import { CreateControlLotDto } from './dto/create-control-lot.dto';
+import { eq } from 'drizzle-orm';
+import { controlLots } from '../drizzle/schema.js';
+
 
 @Injectable()
-export class ControlLotsService {}
+export class ControlLotsService {
+    constructor(private readonly db: DatabaseService) { }
+
+    async create(createControlLotDto: CreateControlLotDto) {
+        const [newLot] = await this.db.db
+            .insert(controlLots)
+            .values({
+                ...createControlLotDto,
+                expirationDate: new Date(createControlLotDto.expirationDate),
+            })
+            .returning();
+        return newLot;
+    }
+}
