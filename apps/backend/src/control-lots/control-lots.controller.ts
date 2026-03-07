484c87880f01b1dfbc2bd9a03c5e52550dc9cae3
Patrick <rekoshehata@gmail.com>
2026-03-07 21:16:06 +0200

    Added Guads and Roles

--- apps/backend/src/control-lots/control-lots.controller.ts
+++ apps/backend/src/control-lots/control-lots.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe, ParseIntPipe, UseGuards } from '@nestjs/common';
 import { ControlLotsService } from './control-lots.service';
 import { CreateControlLotDto } from './dto/create-control-lot.dto';
 import { UpdateControlLotDto } from './dto/update-control-lot.dto';
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/auth/guards/roles.guard';
+import { Roles } from '@/auth/decorators/roles.decorator';
+import { Role } from '@/auth/auth.types';
 
 @Controller('control-lots')
+@UseGuards(JwtAuthGuard, RolesGuard)
 export class ControlLotsController {
     constructor(private readonly controlLotsService: ControlLotsService) { }
 
     @Post()
+
+    @Roles(Role.ADMIN)
     create(@Body(new ValidationPipe()) createControlLotDto: CreateControlLotDto) {
         return this.controlLotsService.create(createControlLotDto);
     }
@@ -22,11 +29,13 @@ export class ControlLotsController {
         return this.controlLotsService.findOne(id);
     }
 
+    @Roles(Role.ADMIN)
     @Patch(':id')
     update(@Param('id', ParseIntPipe) id: number, @Body(new ValidationPipe()) updateControlLotDto: UpdateControlLotDto) {
         return this.controlLotsService.update(id, updateControlLotDto);
     }
 
+    @Roles(Role.ADMIN)
     @Delete(':id')
     remove(@Param('id', ParseIntPipe) id: number) {
         return this.controlLotsService.remove(id);
