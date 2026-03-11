e8cf43e5550825fe33258d0b7f445f069659c632
Patrick <rekoshehata@gmail.com>
2026-03-11 03:28:17 +0200

    Finished the update qc result dto

--- apps/backend/src/qc-results/dto/update-qc-result.dto.ts
+++ apps/backend/src/qc-results/dto/update-qc-result.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateQcResultDto } from './create-qc-result.dto';
+import { IsString, IsOptional } from 'class-validator';
 
-export class UpdateQcResultDto extends PartialType(CreateQcResultDto) {}
+export class UpdateQcResultDto {
+    @IsString()
+    @IsOptional()
+    comments?: string;
+}
