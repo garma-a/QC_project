5f2003b206251c6f8301fc279b3d1bcd45f446e9
Girgis <girgisemad74@gmail.com>
2026-03-20 18:08:49 +0200

    prepare the alerts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,12 +4,12 @@ import { AppService } from '@/app.service';
 import { DatabaseModule } from '@/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { MachinesModule } from '@/machines/machines.module';
-
 import { UsersModule } from '@/users/users.module';
 import { AuthModule } from '@/auth/auth.module';
 import { QcResultsModule } from './qc-results/qc-results.module';
 import { ControlLotsModule } from './control-lots/control-lots.module';
 import { QcTestsModule } from './qc-tests/qc-tests.module';
+import { AlertsModule } from './alerts/alerts.module';
 
 
 
@@ -31,6 +31,7 @@ import { QcTestsModule } from './qc-tests/qc-tests.module';
     QcResultsModule,
     ControlLotsModule,
     QcTestsModule,
+    AlertsModule,
 
   ],
   controllers: [AppController],
