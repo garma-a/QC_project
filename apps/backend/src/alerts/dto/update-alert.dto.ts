0f31153a47b29f42d5e8ae8bf3ba42f37bb9cd1b
Girgis <girgisemad74@gmail.com>
2026-03-22 18:10:35 +0200

    DTO create alerts

--- apps/backend/src/alerts/dto/update-alert.dto.ts
+++ apps/backend/src/alerts/dto/update-alert.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAlertDto } from './create-alert.dto';
 
-export class UpdateAlertDto extends PartialType(CreateAlertDto) {}
+export class UpdateAlertDto extends PartialType(CreateAlertDto) { }
