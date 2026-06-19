153607f24e204e707fc49d77c1635c844335d07a
Girgis <girgisemad74@gmail.com>
2026-06-20 01:25:45 +0300

    delete the testing files

--- apps/backend/test-drizzle-sql-join.js
+++
@@ -1,9 +0,0 @@
-const { sql } = require('drizzle-orm');
-
-try {
-  const ids = [1, 2, 3];
-  const query = sql`SELECT * FROM t WHERE id IN (${sql.join(ids, sql`, `)})`;
-  console.log("SQL:", query.queryChunks);
-} catch(e) {
-  console.error("Error:", e.message);
-}
