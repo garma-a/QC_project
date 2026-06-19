abe4a12efec72a3c5e722814aedb0d75a8284eab
Patrick <rekoshehata@gmail.com>
2026-06-19 05:39:35 +0300

    last fix i swear

--- apps/backend/src/auth/dto/auth-response.dto.ts
+++ apps/backend/src/auth/dto/auth-response.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginResponseDto {
   @ApiProperty({
@@ -20,5 +21,7 @@ export class RefreshTokenDto {
     example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
     description: 'The refresh token obtained during login.',
   })
+  @IsString()
+  @IsNotEmpty()
   refreshToken: string;
 }
